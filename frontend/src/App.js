e1b5de5c1d870d603c6f3491aa58978fbaf2982a
Luigix456 <ditieriluis@gmail.com>
2022-04-05 16:01:07 -0300

    navbar config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import Home from './pages/Home';
 import Footer from "./components/general/Footer"
-
+import Navbar from './components/general/Navbar'
 function App() {
   return (
     <>
-     
+        <Navbar/>
         <Home/>
 
    
-      <Footer />
+        <Footer />
     </>
   );
 }
