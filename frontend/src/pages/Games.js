3d3f49fbb6073c0ef0cda13f8a6e6f41f5c2e3cc
mrain00 <luxassilva@gmail.com>
2022-04-07 10:33:42 -0300

    align cards

--- frontend/src/pages/Games.js
+++ frontend/src/pages/Games.js
@@ -1,52 +1,52 @@
 import React from 'react'
 import { connect } from "react-redux";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import gamesActions from '../redux/actions/gamesActions'
 import '../styles/gamesCards.css'
 
-class Games extends React.Component{
+class Games extends React.Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props)
 	}
 
-	componentDidMount(){
-	        window.scrollTo({top: 0, behavior: "smooth"})
+	componentDidMount() {
+		window.scrollTo({ top: 0, behavior: "smooth" })
 		this.props.fetchGames()
 	}
-	render (){
-		return ( 
+	render() {
+		return (
 			<div className="render-cards">
-				
+
 				<div className="container-cards" >
 					<div className="search-filter">
 
 					</div>
-				{this.props.games && this.props.games.map((game) => 
-					<div className="card" key={game._id}>
-					    <div className="card__header">
-						<img src={process.env.PUBLIC_URL+"/assets/gamesImages/" + game.src} alt="card__image" className="card__image" />
-					    </div>
-					    <div className="card__body">
-							<div className="tag-container">
-							{game.genre.map((genre) => 
-							<span className="tag tag-blue" key={genre}>
-								{genre}
-							</span>
-							)}
+					{this.props.games && this.props.games.map((game) =>
+						<div className="card" key={game._id}>
+							<div className="card__header">
+								<img src={process.env.PUBLIC_URL + "/assets/gamesImages/" + game.src} alt="card__image" className="card__image" />
 							</div>
-						<h4 className='titleGame'>{game.gameName}</h4>
-					    </div>
-					    <div className="card__footer">
+							<div className="card__body">
+								<div className="tag-container">
+									{game.genre.map((genre) =>
+										<span className="tag tag-blue" key={genre}>
+											{genre}
+										</span>
+									)}
+								</div>
+								<h4 className='titleGame'>{game.gameName}</h4>
+							</div>
+							<div className="card__footer">
 
-						<Link to={'/gamesdetails/' + game._id}>
-						<button className="vm">
-						    Explore me
-						</button>
-						</Link>
-					    </div>
-					</div>
-						)}
+								<Link to={'/gamesdetails/' + game._id}>
+									<button className="vm">
+										Explore me
+									</button>
+								</Link>
+							</div>
+						</div>
+					)}
 				</div>
 
 			</div>
@@ -57,14 +57,14 @@ class Games extends React.Component{
 }
 
 const mapStateToProps = (state) => {
-    return {
-	    games:state.gamesReducer.games,
-	    auxiliar:state.gamesReducer.auxiliar
-    }
+	return {
+		games: state.gamesReducer.games,
+		auxiliar: state.gamesReducer.auxiliar
+	}
 }
 
 const mapDispatchToProps = {
-	fetchGames:gamesActions.fetchGames
+	fetchGames: gamesActions.fetchGames
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Games);
