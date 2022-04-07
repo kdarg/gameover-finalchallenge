1e845806e56ebb691fbe524312077f268f773162
BLADIMIR ROJAS <bladimirrojassepulveda@gmail.com>
2022-04-07 17:01:41 -0500

    aboutus

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -6,86 +6,143 @@ export default function AboutUs() {
 
     return ( 
         <>
-        <div className='container_about' style={{backgroundImage:"url(" + process.env.PUBLIC_URL+ "/assets/aboutImages/wall.jpg)"}}>
+        <div className='container_about'>
             <div className='tittle_aboutus'>
-            <h1>Lorem ipsum dolor sit amet</h1>
-            <h3> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident maxime itaque natus facilis cum minima harum. Dolor sed nostrum ad fuga sapiente saepe cupiditate. Ipsum, reiciendis adipisci, provident fugiat cum molestiae libero ut autem ipsa velit aperiam, necessitatibus veniam.</h3>
+            <h1>WE CARE ABOUT GAMES</h1>
+            <h3> GAME OVER is a digital distribution platform with a curated selection of games, a "you buy it, you own it" philosophy, and utmost care about customers.</h3>
             </div>
             <div className='gaming_info'>
             <img src={process.env.PUBLIC_URL+"/assets/aboutImages/searching.png"} alt="img"  className="tittle_about" />    
-            <h1>Hand-picking the best in gaming</h1>
+            <h1 className='tittle_search'>Hand-picking the best in gaming</h1>
             <h2>A selection of great DRM-free games, from modern hits to all-time classics, that you really shouldn't miss.</h2>
                 <div className='gaming_box'>
+                    <div>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/winer.png"} alt="img"  className="icons_about" /> 
-                    <h3>A curated selection of games</h3>
+                    <h3  className='tittle_search'>A curated selection of games</h3>
+                    </div>
+                    <div>
                     <p> From exceptional AAAs, unique indies to the best of classic gaming. Every game is here because we chose it for you.</p>
+                    </div>
                 </div>
                 <div className='gaming_box'>
+                <div>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/surprise.png"} alt="img"  className="icons_about" /> 
-                    <h3>Feeding your inner collector</h3>
-                    <p> Offering games with as many goodies as possible is the GAMEOVER.com way - even if it means exploring our long forgotten basements and attics.</p>
+                    <h3  className='tittle_search'>Feeding your inner collector</h3>
+                    </div>
+                       <div>
+                    <p> Offering games with as many goodies as possible is the GAME OVER way - even if it means exploring our long forgotten basements and attics.</p>
+                    </div> 
                 </div>
                 <div className='gaming_box'>
+                    <div>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/original.png"} alt="img"  className="icons_about" /> 
-                    <h3>Respecting game creators</h3>
+                    <h3  className='tittle_search'>Respecting game creators</h3>
+                    </div>
+                    <div>
                     <p> When you're with us, you can be sure that all games are legitimate, and your purchases support rightful owners and creators.</p>
+                    </div>
                 </div>
                 <div className='gaming_box'>
+                    <div>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/update.png"} alt="img"  className="icons_about" /> 
-                    <h3>Upgrading classics for present-day</h3>
+                    <h3  className='tittle_search'>Upgrading classics for present-day</h3>
+                    </div>
+                    <div>
                     <p> Even if the game is older than you are, we test it thoroughly, fix all the bugs, and apply patches so it runs flawlessly on your next-gen PC and on modern OSs.</p>
+                    </div>
                 </div>
                 <div className='gaming_box'>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/coming.png"} alt="img"  className="icons_about" /> 
-                    <h3>Every game deserves to shine</h3>
-                    <p> As much attention we're giving to pick great games, the same goes to highlighting them on GAMEOVER.com - treating each release as a celebration.</p>
+                    <h3  className='tittle_search'>Every game deserves to shine</h3>
+                    <p> As much attention we're giving to pick great games, the same goes to highlighting them on GAME OVER - treating each release as a celebration.</p>
                 </div>
             </div>
 
             <div className='gaming_info'>
             <img src={process.env.PUBLIC_URL+"/assets/aboutImages/user.png"} alt="img"  className="tittle_about" />
-            <h1>Customer-centric approach</h1>
+            <h1  className='tittle_customer'>Customer-centric approach</h1>
             <h2>A selection of great DRM-free games, from modern hits to all-time classics, that you really shouldn't miss.</h2>
                 <div className='gaming_box'>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/support.png"} alt="img"  className="icons_about" /> 
-                    <h3>Stellar support</h3>
+                    <h3  className='tittle_customer'>Stellar support</h3>
                     <p> We bring you the comfort in knowing that anytime you need help, we're right here for you. Our customer support team works in-house round-the-clock solving all games-related issues.</p>
                 </div>
                 <div className='gaming_box'>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/cashback.png"} alt="img"  className="icons_about" /> 
-                    <h3>No risk, full refunds</h3>
+                    <h3  className='tittle_customer'>No risk, full refunds</h3>
                     <p> Feel safe about your purchase - get your money back if a game doesn't work for you, cancel pre-orders, and get a refund on games in development within 30 days of purchase - no strings attached.</p>
                 </div>
                 <div className='gaming_box'>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/contact.png"} alt="img_contact"  className="icons_about" /> 
-                    <h3>Direct contact with GAMEOVER Team</h3>
-                    <p> Have a question, need help or you just want to talk about great PC games? Reach out on GOG forums, tweet at us or drop us a message on Facebook, and we'll get back to you.</p>
+                    <h3  className='tittle_customer'>team contact</h3>
+                    <p> Have a question, need help or you just want to talk about great PC games? Reach out on GAMEOVER forums, tweet at us or drop us a message on Facebook, and we'll get back to you.</p>
                 </div>
             </div>
 
             <div className='gaming_info'>
             <img src={process.env.PUBLIC_URL+"/assets/aboutImages/more.png"} alt="img_more"  className="tittle_about" />
-            <h1>More</h1>
+            <h1 className='tittle_more'>More</h1>
             <h2>We don't believe in controlling you and your games. Here, you won't be locked out of titles you paid for, or constantly asked to prove you own them - this is DRM-free gaming.</h2>
                 <div className='gaming_box'>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/owner.png"} alt="img_owner"  className="icons_about" /> 
-                    <h3>An optional gaming client</h3>
+                    <h3 className='tittle_more'>Owning the things you buy</h3>
                     <p>We don't believe in controlling you and your games. Here, you won't be locked out of titles you paid for, or constantly asked to prove you own them - this is DRM-free gaming.</p>
                 </div>
                 <div className='gaming_box'>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/settings.png"} alt="img"  className="icons_about" /> 
-                    <h3>An optional gaming client</h3>
+                    <h3 className='tittle_more'>An optional gaming client</h3>
                     <p>  GAME OVER Galaxy is the tailor-made optional client that adds features like cloud saves, update roll-backs, crossplay, achievements, is a convenient way to install & update games, and stay in touch with friends.</p>
                 </div>
                 <div className='gaming_box'>
                 <img src={process.env.PUBLIC_URL+"/assets/aboutImages/miles.png"} alt="img"  className="icons_about" /> 
-                    <h3>Going the extra mile</h3>
-                    <p> We're a company of gamers and we build GOG.com as a platform we, as gamers, want to use - hence our unique approach to DRM, selection of the best in gaming, refunds program and more.</p>
+                    <h3 className='tittle_more'>Always go further with the user</h3>
+                    <p> We're a company of gamers and we build GAME OVER as a platform we, as gamers, want to use - hence our unique approach to DRM, selection of the best in gaming, refunds program and more.</p>
                 </div>
             </div>
-            <div >
-               <h3>Our partners and allies</h3> 
-               <img src={process.env.PUBLIC_URL+"/assets/aboutImages/videojuegos.png"} alt="img"  className="partners" /> 
+            <div className='div_partners' >
+               <h1>Our partners and allies</h1>
+               <div className='images_partner'>
+               <div className='logo_partner'>
+               <img src={process.env.PUBLIC_URL+"/assets/aboutImages/unity.png"} alt="img" 
+                className="partners" />
+                </div>
+                <div className='logo_partner'>
+                <img src={process.env.PUBLIC_URL+"/assets/aboutImages/ea.png"} alt="img" 
+                className="partners" />
+                 </div>
+                 <div className='logo_partner'>
+               <img src={process.env.PUBLIC_URL+"/assets/aboutImages/capcom.png"} alt="img" 
+                className="partners" />
+                </div>
+                <div className='logo_partner'>
+                <img src={process.env.PUBLIC_URL+"/assets/aboutImages/play.png"} alt="img" 
+                className="partners" />
+                 </div>
+                 <div className='logo_partner'>
+               <img src={process.env.PUBLIC_URL+"/assets/aboutImages/ubisoft.png"} alt="img" 
+                className="partners" />
+                </div>
+                <div className='logo_partner'>
+                <img src={process.env.PUBLIC_URL+"/assets/aboutImages/neogeo.png"} alt="img" 
+                className="partners" />
+                 </div>
+                 <div className='logo_partner'>
+               <img src={process.env.PUBLIC_URL+"/assets/aboutImages/2k.png"} alt="img" 
+                className="partners" />
+                </div>
+                <div className='logo_partner'>
+                <img src={process.env.PUBLIC_URL+"/assets/aboutImages/konami.png"} alt="img" 
+                className="partners" />
+                 </div>
+                 <div className='logo_partner'>
+               <img src={process.env.PUBLIC_URL+"/assets/aboutImages/activision.png"} alt="img" 
+                className="partners" />
+                </div>
+                <div className='logo_partner'>
+                <img src={process.env.PUBLIC_URL+"/assets/aboutImages/nintendo.png"} alt="img" 
+                className="partners" />
+                 </div>
+                 </div>
             </div>
         </div>
         </>
