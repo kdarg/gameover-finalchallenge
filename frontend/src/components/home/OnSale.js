59ca93922325511e8cffb4239ca3b01fa0d09c07
JulianaSimonetta <julisimonetta2410@gmail.com>
2022-04-06 23:23:12 -0300

    coming soon & on sale

--- frontend/src/components/home/OnSale.js
+++ frontend/src/components/home/OnSale.js
@@ -4,21 +4,23 @@ const OnSale = () => {
   return (
     <div className="onSaleContainer">
       <div className="onSaleTitle">
-        <h1> ON SALE </h1>
+        <h1> aca va la imagen de on sale </h1>
       </div>
       <div className="onSaleCardsDisplay">
         <div className="onSaleCard">
-          <div className="cardImage">{/* <img></img> */}</div>
+          <div className="cardImage">
+            <img src={process.env.PUBLIC_URL+`/assets/gamesImages/monster_hunter_world.png`}></img>
+          </div>
           <div className="cardInfo">
             <div className="cardName">
                 <h1> Monster Hunter: World</h1>
             </div>
             <div className="cardPrice">
               <div className="cardDiscount">
-                <h2> -60% </h2>
+                <h2> 60% </h2>
               </div>
               <div className="prices">
-                <h3 className="oldPrice"> $33.00 USD</h3>
+                <h4 className="oldPrice"> $33.00 USD</h4>
                 <h3> $3.26 USD</h3>
               </div>
             </div>
@@ -26,17 +28,19 @@ const OnSale = () => {
           </div>
         </div>
         <div className="onSaleCard">
-          <div className="cardImage">{/* <img></img> */}</div>
+          <div className="cardImage">
+            <img src={process.env.PUBLIC_URL+`/assets/gamesImages/hades.png`}></img>
+          </div>
           <div className="cardInfo">
             <div className="cardName">
                 <h1> Hades</h1>
             </div>
             <div className="cardPrice">
               <div className="cardDiscount">
-                <h2> -10% </h2>
+                <h2> 10% </h2>
               </div>
               <div className="prices">
-                <h3 className="oldPrice"> $25.00 USD</h3>
+                <h4 className="oldPrice"> $25.00 USD</h4>
                 <h3> $22.50 USD</h3>
               </div>
             </div>
@@ -44,17 +48,19 @@ const OnSale = () => {
           </div>
         </div>
         <div className="onSaleCard">
-          <div className="cardImage">{/* <img></img> */}</div>
+          <div className="cardImage">
+            <img src={process.env.PUBLIC_URL+`/assets/gamesImages/mario.png`}></img>
+          </div>
           <div className="cardInfo">
             <div className="cardName">
                 <h1> Mario Party Superstars</h1>
             </div>
             <div className="cardPrice">
               <div className="cardDiscount">
-                <h2> -50% </h2>
+                <h2> 50% </h2>
               </div>
               <div className="prices">
-                <h3 className="oldPrice"> $60.00 USD</h3>
+                <h4 className="oldPrice"> $60.00 USD</h4>
                 <h3> $30.00 USD</h3>
               </div>
             </div>
@@ -62,17 +68,19 @@ const OnSale = () => {
           </div>
         </div>
         <div className="onSaleCard">
-          <div className="cardImage">{/* <img></img> */}</div>
+          <div className="cardImage">
+              <img src={process.env.PUBLIC_URL+`/assets/gamesImages/mortalkombat.png`}></img>
+          </div>
           <div className="cardInfo">
             <div className="cardName">
                 <h1> Mortal Kombat 11</h1>
             </div>
             <div className="cardPrice">
               <div className="cardDiscount">
-                <h2> -90% </h2>
+                <h2> 90% </h2>
               </div>
               <div className="prices">
-                <h3 className="oldPrice"> $49.99 USD</h3>
+                <h4 className="oldPrice"> $49.99 USD</h4>
                 <h3> $5.99 USD</h3>
               </div>
             </div>
