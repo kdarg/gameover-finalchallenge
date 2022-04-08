7f9f7b20dd27cbf11448b5d6ebd46650c2356773
unknown <francogaray.dev@gmail.com>
2022-04-08 03:18:53 -0300

    se corrige error de carga de form

--- frontend/src/pages/UsersForm.js
+++ frontend/src/pages/UsersForm.js
@@ -5,18 +5,17 @@ import "../styles/usersForm.css";
 import Paises from "./apiPaises";
 
 const UsersForm = (props) => {
-
   const [selectCountry, setSelectCountry] = useState("unselected");
   function selected(event) {
     setSelectCountry(event.target.value);
   }
 
-  const [lastname, setLastname] = useState('')
-    const [firstname, setFirstname] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [urlimage, setUrlimage] = useState('')
-    const [country, setCountry] = useState('')
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [urlimage, setUrlimage] = useState("");
+  const [country, setCountry] = useState("");
 
   console.log(props);
   const handleSubmit = (event) => {
@@ -32,143 +31,163 @@ const UsersForm = (props) => {
       from: "signup",
     };
     props.signUpUser(userData);
-    console.log(userData)
+    console.log(userData);
   };
-
 
   window.scrollTo({ top: 0, behavior: "smooth" });
 
-  const signUpButton = document.getElementById("signUp");
-  const signInButton = document.getElementById("signIn");
-  const container = document.getElementById("containerUsersForm");
-
-  signUpButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
-  });
-
-  signInButton.addEventListener("click", () => {
+  async function setSignIn(event) {
+    const container = document.getElementById("containerUsersForm");
     container.classList.remove("right-panel-active");
-  });
+  }
+
+  async function setSignUp(event) {
+    const container = document.getElementById("containerUsersForm");
+    container.classList.add("right-panel-active");
+  }
 
   return (
     <>
-      <div class="container-usersForm" id="containerUsersForm">
-        <div class="usersForm-container sign-up-container">
-          <form className="SignForm"  onSubmit={handleSubmit} >
-            <h1 className="h1-usersForm">Create Account</h1>
-            <div class="social-container">
-              <a className="a-usersForm" href="#" class="social a-usersForm">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a className="a-usersForm" href="#" class="social a-usersForm">
-                <i class="fab fa-google-plus-g"></i>
-              </a>
-            </div>
-            <span className="span-usersForm">
-              or use your email for registration
-            </span>
-            <input
-              className="input-usersForm"
-              type="text"
-              placeholder="First name"
-              name="firstname"
-              value={firstname} onChange={ e => setFirstname(e.target.value) }
-            />
-            <input
-              className="input-usersForm"
-              type="text"
-              placeholder="Last name"
-              name="lastName"
-              value={lastname} onChange={ e => setLastname(e.target.value) }
-            />
-              <div className="inputSelect">
-              <select  className="input-usersForm" name="select"
-              value={country} onChange={ e => setCountry(e.target.value) }
-              >
-                <option  value="value" selected disabled>
-                  Select your country
-                </option>
-                {Paises.map((countries, key) => {
-                  return (
-                    <option className="input-usersForm" key={key} value={countries.name}>
-                      {countries.name}
-                    </option>
-                  );
-                })}
-              </select>
+      <div className="backgroundForm">
+        <div class="container-usersForm " id="containerUsersForm">
+          <div class="usersForm-container sign-up-container">
+            <form className="SignForm" onSubmit={handleSubmit}>
+              <h1 className="h1-usersForm">Create Account</h1>
+              <div class="social-container">
+                <a className="a-usersForm" href="#" class="social a-usersForm">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a className="a-usersForm" href="#" class="social a-usersForm">
+                  <i class="fab fa-google-plus-g"></i>
+                </a>
               </div>
-            <input
-              className="input-usersForm"
-              type="text"
-              placeholder="Url Image"
-              name="urlimage"
-              value={urlimage} onChange={ e => setUrlimage(e.target.value) }
-              
-            />
-            <input
-              className="input-usersForm"
-              type="email"
-              placeholder="Email"
-              name="email"
-              value={email} onChange={ e => setEmail(e.target.value) }
-            />
-            <input
-              className="input-usersForm"
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={password} onChange={ e => setPassword(e.target.value) }
-            />
-            <button className="button-usersForm" type="submit">Sign Up</button>
-          </form>
-        </div>
-        <div class="usersForm-container sign-in-container">
-          <form className="SignForm" action="#">
-            <h1 className="h1-usersForm">Sign in</h1>
-            <div class="social-container">
-              <a className="a-usersForm" href="#" class="social a-usersForm">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a className="" href="#" class="social a-usersForm">
-                <i class="fab fa-google-plus-g"></i>
-              </a>
-            </div>
-            <span className="span-usersForm">or use your account</span>
-            <input
-              className="input-usersForm"
-              type="email"
-              placeholder="Email"
-            />
-            <input
-              className="input-usersForm"
-              type="password"
-              placeholder="Password"
-            />
-            <a className="a-usersForm" href="#">
-              Forgot your password?
-            </a>
-            <button className="button-usersForm">Sign In</button>
-          </form>
-        </div>
-        <div class="overlay-container">
-          <div class="overlay">
-            <div class="overlay-panel overlay-left">
-              <h1 className="h1-usersForm">Welcome Back!</h1>
-              <p className="p-usersForm">
-                To keep connected with us please login with your personal info
-              </p>
-              <button className="button-usersForm ghost" id="signIn">
-                Sign In
-              </button>
-            </div>
-            <div class="overlay-panel overlay-right">
-              <h1 className="h1-usersForm">Hello, Friend!</h1>
-              <p className="p-usersForm">
-                Enter your personal details and start journey with us
-              </p>
-              <button className="button-usersForm ghost" id="signUp">
+              <span className="span-usersForm">
+                or use your email for registration
+              </span>
+              <input
+                className="input-usersForm"
+                type="text"
+                placeholder="First name"
+                name="firstname"
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+              <input
+                className="input-usersForm"
+                type="text"
+                placeholder="Last name"
+                name="lastName"
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+              />
+              <div className="inputSelect">
+                <select
+                  className="input-usersForm"
+                  name="select"
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                >
+                  <option value="value" selected disabled>
+                    Select your country
+                  </option>
+                  {Paises.map((countries, key) => {
+                    return (
+                      <option
+                        className="input-usersForm"
+                        key={key}
+                        value={countries.name}
+                      >
+                        {countries.name}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <input
+                className="input-usersForm"
+                type="text"
+                placeholder="Url Image"
+                name="urlimage"
+                value={urlimage}
+                onChange={(e) => setUrlimage(e.target.value)}
+              />
+              <input
+                className="input-usersForm"
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                className="input-usersForm"
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="button-usersForm" type="submit">
                 Sign Up
               </button>
+            </form>
+          </div>
+          <div class="usersForm-container sign-in-container">
+            <form className="SignForm" action="#">
+              <h1 className="h1-usersForm">Sign in</h1>
+              <div class="social-container">
+                <a className="a-usersForm" href="#" class="social a-usersForm">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a className="" href="#" class="social a-usersForm">
+                  <i class="fab fa-google-plus-g"></i>
+                </a>
+              </div>
+              <span className="span-usersForm">or use your account</span>
+              <input
+                className="input-usersForm"
+                type="email"
+                placeholder="Email"
+              />
+              <input
+                className="input-usersForm"
+                type="password"
+                placeholder="Password"
+              />
+              <a className="a-usersForm" href="#">
+                Forgot your password?
+              </a>
+              <button className="button-usersForm">Sign In</button>
+            </form>
+          </div>
+          <div class="overlay-container">
+            <div class="overlay">
+              <div class="overlay-panel overlay-left">
+                <h1 className="h1-usersForm">Welcome Back!</h1>
+                <p className="p-usersForm">
+                  To keep connected with us please login with your personal info
+                </p>
+                <button
+                  className="button-usersForm ghost"
+                  id="signIn"
+                  onClick={setSignIn}
+                >
+                  Sign In
+                </button>
+              </div>
+              <div class="overlay-panel overlay-right">
+                <h1 className="h1-usersForm">Hello, Friend!</h1>
+                <p className="p-usersForm">
+                  Enter your personal details and start journey with us
+                </p>
+                <button
+                  className="button-usersForm ghost"
+                  id="signUp"
+                  onClick={setSignUp}
+                >
+                  Sign Up
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -182,7 +201,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = {
-  signUpUser: userActions.signUpUser
+  signUpUser: userActions.signUpUser,
 };
 
 export default connect(null, mapDispatchToProps)(UsersForm);
