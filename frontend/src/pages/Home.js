0722be07eb105b1f526142095126b06a05c10879
Luigix456 <ditieriluis@gmail.com>
2022-04-08 00:55:51 -0300

    comments final

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,7 @@ import Carousel from './Carousel'
 import OnSale from "../components/home/OnSale"
 import ComingSoon from "../components/home/ComingSoon"
 import Top from '../components/home/Top'
-
+import Comments from '../components/home/Comments'
 import '../styles/gamesCards.css'
 
 const Home = () => {
@@ -16,7 +16,8 @@ const Home = () => {
             <Carousel/>
             <OnSale />
 	    {/*<ComingSoon />*/}
-	    </div>
+            <Comments/>
+        </div>
         </>
     );
 }
