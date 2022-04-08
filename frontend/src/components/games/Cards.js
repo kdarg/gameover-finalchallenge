3f75524c128812f36d631a6c5eccf6be48c83e9f
Karen Arguello <karuarguello@gmail.com>
2022-04-08 10:51:11 -0300

    arreglo de classes repetidas

--- frontend/src/components/games/Cards.js
+++ frontend/src/components/games/Cards.js
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import { connect } from "react-redux";
-import '../../styles/gamesCards.css'
-const Cards = (props) => {
+// import React, { useEffect, useState } from 'react'
+// import { connect } from "react-redux";
+// import '../../styles/gamesCards.css'
+// const Cards = (props) => {
 
-    return (
-        <>
-            <div className="container-cards">
-                <div className="card">
-                    <div className="card__header">
-                        <img src="https://www.mundoplayers.com/wp-content/uploads/2019/05/Mortal-Kombat-11_4.jpg" alt="card__image" className="card__image" />
-                    </div>
-                    <div className="card__body">
-                        <span className="tag tag-blue">Fight</span>
-                        <h4 className='titleGame'>Mortal Kombat 11</h4>
-                    </div>
-                    <div className="card__footer">
-                        <button className="vm">
-                            Explore me
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </>
+//     return (
+//         <>
+//             <div className="container-cards">
+//                 <div className="card__gamestab">
+//                     <div className="card__header">
+//                         <img src="https://www.mundoplayers.com/wp-content/uploads/2019/05/Mortal-Kombat-11_4.jpg" alt="card__image" className="card__image" />
+//                     </div>
+//                     <div className="card__body">
+//                         <span className="tag tag-blue">Fight</span>
+//                         <h4 className='titleGame'>Mortal Kombat 11</h4>
+//                     </div>
+//                     <div className="card__footer">
+//                         <button className="vm">
+//                             Explore me
+//                         </button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
 
-    );
-}
+//     );
+// }
 
-const mapStateToProps = (state) => {
-    return {
+// const mapStateToProps = (state) => {
+//     return {
 
-    }
-}
+//     }
+// }
 
-const mapDispatchToProps = {
+// const mapDispatchToProps = {
 
-}
+// }
 
-export default connect(null, null)(Cards);
+// export default connect(null, null)(Cards);
