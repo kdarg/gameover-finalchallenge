4fd41fab211b024cf58db6cc209b56a2210585fb
Karen Arguello <karuarguello@gmail.com>
2022-04-09 01:50:03 -0300

    genres fixed

--- frontend/src/pages/Games.js
+++ frontend/src/pages/Games.js
@@ -10,7 +10,22 @@ import NoGames from '../components/general/NoGames';
 class Games extends React.Component {
 
 	state = {
-		genreColors:['blue', 'red', 'green']
+		genreColors: {	
+			'Action': '#00C3F6', 
+			'Action RPG':'#d84185',
+			'Adventure': '#BA40E1', 
+			'Building':'blue',
+			'Fantasy':'#2ed4ad',
+			'Historical': 'rgb(0, 171, 46)',
+			'Indies':'rgb(101, 100, 97)',
+			'Multiplayer':'#ec8837',
+			'MOBA':'red',
+			'Mystery': 'purple',
+			'Open World':'#ec378e', 
+			'Party':'rgb(147, 153, 255)',
+			'Strategy':'#8ed236',
+		}
+		
 	}
 
 	componentDidMount() {
@@ -39,7 +54,7 @@ class Games extends React.Component {
 								<div className="card__body">
 									<div className="tag-container">
 										{game.genre.map((genre) =>
-										<span className="tag tag-blue" key={genre} style={{background:this.state.genreColors[game.genre.indexOf(genre)]}}>
+										<span className="tag tag-blue" key={genre} style={{background:this.state.genreColors[genre]}}>
 												{genre}
 											</span>
 										)}
