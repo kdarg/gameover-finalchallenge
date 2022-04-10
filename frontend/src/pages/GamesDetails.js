2fb1ba3bfad142020f803bb494d7b93eadafdf6f
mrain00 <luxassilva@gmail.com>
2022-04-10 04:15:10 -0300

    process detail

--- frontend/src/pages/GamesDetails.js
+++ frontend/src/pages/GamesDetails.js
@@ -9,70 +9,84 @@ import gamesActions from '../redux/actions/gamesActions'
 const GamesDetails = (props) => {
 
     console.log(props)
-    
+
     const { game } = props
 
     window.scrollTo({ top: 0, behavior: "smooth" })
 
     const { id } = useParams()
 
-  useEffect(() => {
-    props.fetchGame(id)
-    // eslint-disable-next-line     
-  }, []);
+    useEffect(() => {
+        props.fetchGame(id)
+        // eslint-disable-next-line     
+    }, []);
 
     return (
-        <div className="details-container">
-            <div className="product-details">
-                <ul className="details-menu-list">
-                    <li className="text-home-container">GENRES</li>
-                    <li className="text-product-details">Batman: The Enemy Within</li>
-                    <div className='price-container'>
-                        <h2>$50</h2>
-                        <button className="btn add-to-cart">Add to cart</button>
-                    </div>
-                </ul>
-            </div>
-            <div className="product-container">
-                <div className="product-container-child">
-                <div className="container-image">
-                        <img src="http://localhost:3000/assets/gamesImages/batman.png" alt='img' className='product-image' />
-                        <div className="container-images">
-                            <img src="http://localhost:3000/assets/gamesImages/batman.png" />
-                            <img src="http://localhost:3000/assets/gamesImages/batman.png" />
-                            <img src="http://localhost:3000/assets/gamesImages/batman.png" />
-                            <img src="http://localhost:3000/assets/gamesImages/batman.png" />
-                        </div>
-                    </div>
-                    <div className="product-detail-info">
-                        <div>
-                            <h4>Workson:</h4>
-                            <p></p>
-                            <h4>Company:</h4>
-                            <p></p>
-                            <h4>Size:</h4>
-                            <p></p>
-                            <ul className='list-req'>{/* MAP DE SYSTEM REQ */}
-                                <li className="product-item">processor: 'Dual core 2.6 GHz Intel Pentium D or AMD Athlon 64 X2 3800+'</li>
-                                <li className="req-item">memory: '2 GB RAM'</li>
-                                <li className="req-item">graphics: 'ATI/NVIDIA 256 MB DirectX 10.0–compliant video card or DirectX 9.0–compliant card with Shader Model 3.0 or higher'</li>
-                                <li className="req-item">storage:'8 GB'</li>
+        <>
+            {game.games && (
+                <>
+                    <div className="details-container">
+                        <div className="product-details">
+                            <ul className="details-menu-list">
+                                <ul className='text-home-container'>
+                                    {game.games.genre && game.games.genre.map((genres) =>
+                                        <li className="text-genres" key={genres}>{genres}</li>
+                                    )}
+                                </ul>
+                                <li className="text-product-details">{game.games.gameName}</li>
+                                <div className='price-container'>
+                                    <h2>$ {game.games.price}</h2>
+                                    <button className="btn add-to-cart">Add to cart</button>
+                                </div>
                             </ul>
                         </div>
+                        <div className="product-container">
+                            <div className="product-container-child">
+                                <div className="container-image">
+                                    <img src={process.env.PUBLIC_URL + "/assets/gamesImages/" + game.games.src} alt='img' className='product-image' />
+                                    <div className="container-images">
+                                        {game.games.images.map((imgs) =>
+                                            <img src={process.env.PUBLIC_URL + "/assets/productImages/" + imgs} />
+                                        )}
+                                    </div>
+                                </div>
+                                <div className="product-detail-info">
+                                    <div>
+                                        <h4>Workson:
+                                            {game.games.workson}
+                                        </h4>
+                                        <p></p>
+                                        <h4>Company:
+                                            {game.games.company}
+                                        </h4>
+                                        <p></p>
+                                        <h4>Size:
+                                            {game.games.size}
+                                        </h4>
+                                        <p></p>
+                                        <div className='list-req'>
+                                            {/* {game.games.requirements.map((req)=>
+                                                <p className="product-item">
+                                                    {req.processador}
+                                                </p>)} */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="product-container-child-2">
+                                <h3>About the game</h3>
+                                <p>{game.games.description}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="product-container-child-2">
-                    <h3>About the game</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-            </div>
-        </div>
-
+                </>
+            )}
+        </>
     );
 }
 
 const mapStateToProps = (state) => {
-    return{
+    return {
         game: state.gamesReducer.game,
     }
 }
