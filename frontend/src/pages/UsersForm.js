8eae4aafcbd53fc33545c51c55bcad5ccf5dc551
unknown <francogaray.dev@gmail.com>
2022-04-11 02:55:06 -0300

    formulario responsivo

--- frontend/src/pages/UsersForm.js
+++ frontend/src/pages/UsersForm.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import userActions from "../redux/actions/usersActions";
 import "../styles/usersForm.css";
-import Paises from "./apiPaises"; 
+import Paises from "./apiPaises";
 import Swal from "sweetalert2";
 
 const UsersForm = (props) => {
@@ -70,7 +70,25 @@ const UsersForm = (props) => {
       props.signInUser(logedUser);
     }
   };
+  //FUNCTION CARD FORM
+  document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("flip-card-btn-turn-to-back").style.visibility =
+      "visible";
+    document.getElementById("flip-card-btn-turn-to-front").style.visibility =
+      "visible";
 
+    document.getElementById("flip-card-btn-turn-to-back").onclick =
+      function () {
+        document.getElementById("flip-card").classList.toggle("do-flip");
+      };
+
+    document.getElementById("flip-card-btn-turn-to-front").onclick =
+      function () {
+        document.getElementById("flip-card").classList.toggle("do-flip");
+      };
+  });
+
+  //Function Scroll
   async function setSignIn(event) {
     const container = document.getElementById("containerUsersForm");
     container.classList.remove("right-panel-active");
@@ -83,7 +101,7 @@ const UsersForm = (props) => {
 
   return (
     <>
-      <div className="backgroundForm">
+      <div className="backgroundForm fullForm">
         <div className="container-usersForm " id="containerUsersForm">
           <div className="usersForm-container sign-up-container">
             <form className="SignForm" onSubmit={handleSubmit}>
@@ -238,13 +256,157 @@ const UsersForm = (props) => {
           </div>
         </div>
       </div>
+
+      {/* CARD FORM RESPONSIVE */}
+
+      <div className="backgroundForm formResponsive">
+        <div className="flip-card-3D-wrapper">
+          <div id="flip-card" className="container-usersFormResp ">
+            <div className="flip-card-front usersFormResp-container resp-sign-in-container">
+              <form className="SignForm" onSubmit={handleSubmitSignIn}>
+                <h1 className="h1-usersForm">Sign in</h1>
+                <div className="social-container">
+                  <a className="a-usersForm social a-usersForm" href="#">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a className="social a-usersForm" href="#">
+                    <i className="fab fa-google-plus-g"></i>
+                  </a>
+                </div>
+                <span className="span-usersForm">or use your account</span>
+                <input
+                  className="input-usersForm"
+                  type="email"
+                  placeholder="Email"
+                  value={emailSignIn}
+                  onChange={(e) => setEmailSignIn(e.target.value)}
+                />
+                <input
+                  className="input-usersForm"
+                  type="password"
+                  placeholder="Password"
+                  value={passwordSignIn}
+                  onChange={(e) => setPasswordSignIn(e.target.value)}
+                />
+                <button
+                  className="button-usersForm"
+                  type="submit"
+                  value="Sign in"
+                >
+                  Sign In
+                </button>
+                <button
+                  type="button"
+                  id="flip-card-btn-turn-to-back"
+                  className="button-usersForm flip-card-btn-turn-to-back"
+                >
+                  Sign Up
+                </button>
+              </form>
+            </div>
+            <div className="flip-card-back usersFormResp-container resp-sign-up-container">
+              <form className="SignForm" onSubmit={handleSubmit}>
+                <h1 className="h1-usersForm">Create Account</h1>
+                <div className="social-container">
+                  <a className="a-usersForm social a-usersForm" href="#">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a className="a-usersForm social a-usersForm" href="#">
+                    <i className="fab fa-google-plus-g"></i>
+                  </a>
+                </div>
+                <span className="span-usersForm">
+                  or use your email for registration
+                </span>
+                <input
+                  className="input-usersForm"
+                  type="text"
+                  placeholder="First name"
+                  name="firstname"
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                />
+                <input
+                  className="input-usersForm"
+                  type="text"
+                  placeholder="Last name"
+                  name="lastName"
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                />
+                <div className="inputSelect">
+                  <select
+                    className="input-usersForm"
+                    name="select"
+                    value={country}
+                    onChange={(e) => setCountry(e.target.value)}
+                  >
+                    <option value="value" defaultValue disabled>
+                      Select your country
+                    </option>
+                    {Paises.map((countries, key) => {
+                      return (
+                        <option
+                          className="input-usersForm"
+                          key={key}
+                          value={countries.name}
+                        >
+                          {countries.name}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                <input
+                  className="input-usersForm"
+                  type="text"
+                  placeholder="Url Image"
+                  name="urlimage"
+                  value={urlimage}
+                  onChange={(e) => setUrlimage(e.target.value)}
+                />
+                <input
+                  className="input-usersForm"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  className="input-usersForm"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button
+                  className="button-usersForm"
+                  type="submit"
+                  value="Sign up"
+                >
+                  Sign Up
+                </button>
+                <button
+                  id="flip-card-btn-turn-to-front"
+                  className="button-usersForm flip-card-btn-turn-to-back"
+                  type="button"
+                >
+                  Sign In
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
-	  user:state.usersReducer.user
+    user: state.usersReducer.user,
   };
 };
 
