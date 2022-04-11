485ea0086cfa1a68ce2f39194585f92bef82a8bd
Luigix456 <ditieriluis@gmail.com>
2022-04-11 12:30:22 -0300

    correccion call to action

--- frontend/src/components/home/callToAction.js
+++ frontend/src/components/home/callToAction.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../../styles/callToAction.css';
 import '../../styles/buttonHero.css';
+import '../../styles/callToAction.css';
 import { Link } from "react-router-dom";
 
 function CallToAction() {
@@ -9,17 +9,16 @@ function CallToAction() {
     <>
         <div className="hero-container">
             <div className="hero1">
-            </div>
-            <div className='product-image hero2'>
-              <img className='img-hero' src={process.env.PUBLIC_URL + "/hero-image3.png"}></img>
+            <img className='img-hero' src={process.env.PUBLIC_URL + "/hero-image3.png"}></img>
             </div>
             <div className="hero-text">
-            {/* <img className="logo-hero" src={process.env.PUBLIC_URL+"/assets/footerImages/logofinal.png"}></img> */}
-                <p className='hero-paragraph'>Looking for the best games and prices? You've come to the right place!</p>
-                
-                <Link to="/games" className='text-reset'>
-                  <div className='hero-anchor' href="#scroll3"><button className='hero-button hero-button2'>Check Games</button></div>
-                </Link>
+              <div className='container-paragraphs'>
+                <p className='hero-paragraph'>¿Looking for the best games and prices?</p>
+                <p className='hero-paragraph'>¡You've come to the right place!</p>
+              </div>
+              <Link to="/games" className='text-reset'>
+                <div className='hero-anchor' href="#scroll3"><button className='hero-button hero-button2'>Check Games</button></div>
+              </Link>
             </div>
         </div>
     </>
