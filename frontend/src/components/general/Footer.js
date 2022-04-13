d980856a89aa6282a0f12fdd120a2958e3028f87
mrain00 <luxassilva@gmail.com>
2022-04-13 14:20:09 -0300

    full facha

--- frontend/src/components/general/Footer.js
+++ frontend/src/components/general/Footer.js
@@ -3,33 +3,37 @@ import '../../styles/footer.css';
 import { MDBFooter } from 'mdb-react-ui-kit';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import { Link } from "react-router-dom";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Footer() {
   return (
-    <MDBFooter  className=' p-1  text-lg-start text-light'>
+    <MDBFooter className=' p-1  text-lg-start text-light'>
 
       <section className='height-container-footer'>
         <div className='container text-center text-md-start mt-5'>
           <div className='probar mt-3'>
             <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-2 random'>
 
-              <img src={process.env.PUBLIC_URL+"/assets/footerImages/logofinal.png"} alt="img"  className="logofooter" />
+              <img src={process.env.PUBLIC_URL + "/assets/footerImages/logofinal.png"} alt="img" className="logofooter" />
             </div>
 
             <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4 font-footer-x'>Pages</h6>
               <div className='pad-links'>
                 <Link to={"/home"} className='text-reset font-footer-x'>
-                <img src={process.env.PUBLIC_URL+"/assets/footerImages/homex.png"} alt="img" className="iconsfooter"/> Home</Link>
+                  <img src={process.env.PUBLIC_URL + "/assets/footerImages/homex.png"} alt="img" className="iconsfooter" /> Home</Link>
               </div>
               <div className='pad-links'>
-                <Link to="/games" className='text-reset font-footer-x'> <img src={process.env.PUBLIC_URL+"/assets/footerImages/consolex.png"} alt="img"  className="iconsfooter"/>Games</Link>
+                <Link to="/games" className='text-reset font-footer-x'> <img src={process.env.PUBLIC_URL + "/assets/footerImages/consolex.png"} alt="img" className="iconsfooter" />Games</Link>
               </div>
               <div className='pad-links'>
-                <Link to="/user" className='text-reset font-footer-x'> <img src={process.env.PUBLIC_URL+"/assets/footerImages/userx.png"} alt="img"  className="iconsfooter" />User</Link>
+                <Link to="/user" className='text-reset font-footer-x'> <img src={process.env.PUBLIC_URL + "/assets/footerImages/userx.png"} alt="img" className="iconsfooter" />User</Link>
               </div>
               <div className='pad-links'>
-                <Link to="/aboutus" className='text-reset font-footer-x'><img src={process.env.PUBLIC_URL+"/assets/footerImages/peoplex.png"} alt="img" className="iconsfooter"/> About Us </Link>
+                <Link to="/aboutus" className='text-reset font-footer-x'><img src={process.env.PUBLIC_URL + "/assets/footerImages/peoplex.png"} alt="img" className="iconsfooter" /> About Us </Link>
               </div>
             </div>
 
@@ -40,12 +44,11 @@ export default function Footer() {
               <p className='font-footer-x'> + 01 234 567 88 </p>
               <p className='font-footer-x'> + 01 234 567 89 </p>
               <div className='iconsocial'>
-              <a href="https://www.facebook.com/" target='blank'><img src={process.env.PUBLIC_URL+"/assets/footerImages/facebook.png"} alt="img"  className="iconsocials" /></a>
-                <a href="https://www.instagram.com/accounts/login/?hl=es&sour..." target='blank'> <img  src={process.env.PUBLIC_URL+"/assets/footerImages/instagram.png"} alt="img"  className="iconsocials" /></a>
-                <a href= "https://www.youtube.com/" target='blank'> <img src={process.env.PUBLIC_URL+"/assets/footerImages/youtube.png"} alt="img"  className="iconsocials" /></a>
-                <a href="https://mobile.twitter.com/i/flow/login" target='blank'><img src={process.env.PUBLIC_URL+"/assets/footerImages/twitter.png"} alt="img"  className="iconsocials" /></a>
-                <a href="https://discord.com/login" target='blank'><img src={process.env.PUBLIC_URL+"/assets/footerImages/discord.png"} alt="img"  className="iconsocials" /></a>
-                </div>
+                 <FacebookIcon/>
+                 <TwitterIcon/>
+                 <YouTubeIcon/>
+                 <InstagramIcon/>
+              </div>
             </div>
           </div>
         </div>
