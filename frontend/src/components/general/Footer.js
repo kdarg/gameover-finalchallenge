a03e15408a18920649e10715136302255d08515c
mrain00 <luxassilva@gmail.com>
2022-04-13 14:29:55 -0300

    delete home.css

--- frontend/src/components/general/Footer.js
+++ frontend/src/components/general/Footer.js
@@ -3,10 +3,16 @@ import '../../styles/footer.css';
 import { MDBFooter } from 'mdb-react-ui-kit';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import { Link } from "react-router-dom";
+
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
+
+import HomeIcon from '@mui/icons-material/Home';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import PersonIcon from '@mui/icons-material/Person';
+import GroupIcon from '@mui/icons-material/Group';
 
 export default function Footer() {
   return (
@@ -24,16 +30,16 @@ export default function Footer() {
               <h6 className='text-uppercase fw-bold mb-4 font-footer-x'>Pages</h6>
               <div className='pad-links'>
                 <Link to={"/home"} className='text-reset font-footer-x'>
-                  <img src={process.env.PUBLIC_URL + "/assets/footerImages/homex.png"} alt="img" className="iconsfooter" /> Home</Link>
+                 <HomeIcon className="iconsfooter" /> Home</Link>
               </div>
               <div className='pad-links'>
-                <Link to="/games" className='text-reset font-footer-x'> <img src={process.env.PUBLIC_URL + "/assets/footerImages/consolex.png"} alt="img" className="iconsfooter" />Games</Link>
+                <Link to="/games" className='text-reset font-footer-x'> <SportsEsportsIcon className="iconsfooter" />Games</Link>
               </div>
               <div className='pad-links'>
-                <Link to="/user" className='text-reset font-footer-x'> <img src={process.env.PUBLIC_URL + "/assets/footerImages/userx.png"} alt="img" className="iconsfooter" />User</Link>
+                <Link to="/user" className='text-reset font-footer-x'> <PersonIcon className="iconsfooter" />User</Link>
               </div>
               <div className='pad-links'>
-                <Link to="/aboutus" className='text-reset font-footer-x'><img src={process.env.PUBLIC_URL + "/assets/footerImages/peoplex.png"} alt="img" className="iconsfooter" /> About Us </Link>
+                <Link to="/aboutus" className='text-reset font-footer-x'><GroupIcon className="iconsfooter" /> About Us </Link>
               </div>
             </div>
 
