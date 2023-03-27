371fbf67d8612fa21a30b1d797a6edb50ad4354f
Karen Arguello <karuarguello@gmail.com>
2023-03-27 23:07:17 +0200

    Added vercel config file for backend

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,4 @@ app.use(fileUpload())
 app.use(express.json());
 app.use(passport.initialize());
 app.use('/api', Router);
-
-
-// HOST HEROKU
-
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"))
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.join(__dirname + "/client/build/index.html"))
-//     })
-//     }
-
 app.listen(process.env.PORT || 4000, process.env.HOST || "0.0.0.0", () => console.log(`Server listening on port ${process.env.PORT || 4000}`))
-
